947a0deb863fb5f685d7383a436ea9bd7d2e5301
nhestrompia <nhestrompia@gmail.com>
2022-12-25 00:00:21 +0300

    server bug

--- server/index.js
+++ server/index.js
@@ -11,7 +11,8 @@ const server = http.createServer(app)
 const io = new Server(server, {
   cors: {
     // origin: "http://localhost:3000",
-    origin: "http://zkblackjack.onrender.com/",
+    // origin: "http://zkblackjack.onrender.com/",
+    origin: "*",
     methods: ["GET", "POST"],
   },
 })
